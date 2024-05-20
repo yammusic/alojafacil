ead15853877b260ace5d48bac3c985a29c94d308
Yeison Molina <yeison.molina91@gmail.com>
2024-05-20 02:03:12 -0500

    Api changes

--- src/app/(routes)/(api)/api/auth/login/route.ts
+++ src/app/(routes)/(api)/api/auth/login/route.ts
@@ -1,21 +1,17 @@
 import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { setCookie } from 'cookies-next'
 import '@/domain/polyfills'
 
 import {
   NotFoundException,
+  UnauthorizedException,
   apiMiddleware,
   responseApiException,
   responseApiSuccess,
 } from '@/domain/providers'
 import { getUser } from '@/domain/db'
 import { requestLoginValidator } from './validator'
-
-type LoginResponse = {
-  content: {
-    message: string
-    user: any
-  }
-}
 
 export async function POST(req: Request) {
   const res = NextResponse
@@ -32,9 +28,20 @@ export async function POST(req: Request) {
     const user = await getUser(username)
     if (!user) { throw new NotFoundException() }
 
-    await user.sign()
+    const { accessToken, expiredAt } = await user.sign()
+    const isExpired = Number(expiredAt) < Date.now()
+    if (!accessToken || isExpired) { throw new UnauthorizedException() }
 
-    return responseApiSuccess<LoginResponse>(res, {
+    setCookie('session', accessToken, {
+      cookies,
+      httpOnly: true,
+      secure: true,
+      expires: new Date(Number(expiredAt)),
+      sameSite: 'lax',
+      path: '/',
+    })
+
+    return responseApiSuccess(res, {
       content: {
         message: 'Login successfully!',
         user: await user.json(),
---
+++ src/app/(routes)/(api)/api/auth/logout/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { deleteCookie } from 'cookies-next'
+
+import {
+  apiMiddleware,
+  responseApiException,
+  responseApiSuccess,
+} from '@/domain/providers'
+import { Session, SessionStatus } from '@/domain/db'
+
+export async function DELETE(req: Request) {
+  const res = NextResponse
+  const params = {}
+
+  try {
+    await apiMiddleware(req, params, res, {
+      authorization: true,
+      only: ['DELETE'],
+    })
+
+    const accessToken = req.headers.get('authorization')?.split(' ')[1]
+    const session = await Session.findOne({ accessToken }) as Session
+    session.status = SessionStatus.EXPIRED
+    session.updatedAt = new Date()
+    await session.save()
+
+    deleteCookie('session', { cookies })
+
+    return responseApiSuccess(res, {
+      content: {
+        message: 'Logout successfully!',
+      },
+    })
+  } catch (exception) {
+    console.error(exception)
+    return responseApiException(res, exception)
+  }
+}
