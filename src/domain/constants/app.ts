8ce92596626db860749c6af379300a08a79c6006
Yeison Molina <yeison.molina91@gmail.com>
2024-05-20 02:09:02 -0500

    Add constants

---
+++ src/domain/constants/app.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = 'Aloja Fácil'
+export const DRAWER_WIDTH = 260
+export const DRAWER_WIDTH_SMALL = 72
--- src/domain/constants/env.ts
+++ src/domain/constants/env.ts
@@ -1,2 +1,4 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
+
+export const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY
